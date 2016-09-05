6f7fb17a0772580ece5cc0504b98a3d9f19a3f68
samuel howell <samuel howell@T3500-FDD012>
2016-09-05 17:31:40 +0100

    fakes generated

--- Command_and_Conquer_Style_RTS.Tests/IPositionableTest.cs
+++ Command_and_Conquer_Style_RTS.Tests/IPositionableTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Command_and_Conquer_Style_RTS.Interfaces.Map;
 using Command_and_Conquer_Style_RTS.Enums.Terrain;
 using Command_and_Conquer_Style_RTS.Interfaces.Unit.CommonUnitInterfaces;
+using Command_and_Conquer_Style_RTS.Interfaces.Unit.CommonUnitInterfaces.Fakes;
 
 namespace Command_and_Conquer_Style_RTS.Tests
 {
@@ -14,19 +15,6 @@ namespace Command_and_Conquer_Style_RTS.Tests
         public void GetTileFromMap()
         {
             IPosition pos = testsubject.Position;
-            ITile tile = testsubject.GetTileFromMap();
-
-            IPosition testposition = tile.Position;
-
-            Assert.AreEqual(pos, testposition);
-        }
-        [TestMethod]
-        public void CanAddItemEmpty()
-        {
-            IUnit a = new StubIUnit
-            {
-
-            };
             ITile tile = testsubject.GetTileFromMap();
 
             IPosition testposition = tile.Position;
---
+++ Command_and_Conquer_Style_RTS.Tests/ITileTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Command_and_Conquer_Style_RTS.Interfaces.Map;
+using Command_and_Conquer_Style_RTS.Interfaces.Unit.CommonUnitInterfaces;
+using Command_and_Conquer_Style_RTS.Interfaces.Unit.CommonUnitInterfaces.Fakes;
+
+namespace Command_and_Conquer_Style_RTS.Tests
+{
+    [TestClass]
+    public class ITileTest
+    {
+        private ITile testsubject;
+        [TestMethod]
+        public void CanAddItemToUnitList()
+        {
+            IUnit testingunit1 = new StubIUnit
+            {
+                IsCollidableGet = () => true
+            };
+            testsubject.AddItem(testingunit1);
+            Assert.IsTrue(testsubject.UnitList.Contains(testingunit1));
+         }
+        [TestMethod]
+        public void CanAddItemEmpty()
+        {
+            IUnit testingunit1 = new StubIUnit
+            {
+                IsCollidableGet = () => true
+            };
+            IUnit testingunit2 = new StubIUnit
+            {
+                IsCollidableGet = () => true
+            };
+            testsubject.AddItem(testingunit1);
+            Assert.IsFalse(testsubject.UnitList.Contains(testingunit1));
+        }
+    }
+}
